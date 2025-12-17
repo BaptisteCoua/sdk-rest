32aa79dcf56a1c48a8efcd61b272e0e4d51c66ab
baptisteCoua <couavoux.b@gmail.com>
2025-12-17 14:46:35 +0100

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-btn @click="toto">Bouton</v-btn>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const toto = () => {
+    console.log("'adazd'")
+    console.log("'adazd'")
+    console.log("'adazd'")
+    console.log("'adazd'")
+    console.log("'adazd'")
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', 'vuetify-nuxt-module'],
+  vuetify: {
+    moduleOptions: {
+      /* module specific options */
+    },
+    vuetifyOptions: {
+      /* vuetify options */
+    }
+  }
+})
